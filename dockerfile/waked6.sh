67b5eb3285d48783c88ab69bd4caf2dc0a79e708
k f <fk506.cni@gmail.com>
2025-04-15 15:00:17 +0900

    dc

---
+++ dockerfile/waked6.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  docker save x1 -o x1.tar
+#  docker load -i x1.tar
+
+dhost=/home/fk506cni/hdd/dc_exp/testnote
+nhost=/home/fk506cni/hdd/dc_exp/vbs
+dguest=/home/jovyan/share1
+nguest=/home/jovyan/share2
+
+ docker container run \
+    --rm -p 8888:8888 \
+    -v $dhost:$dguest \
+    -v $nhost:$nguest \
+    -it x6 \
+    /bin/bash /usr/local/bin/doc_start.sh
